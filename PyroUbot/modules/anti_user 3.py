906bc052e6343cfa4485d1d4862459fcd43baa01
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2024-11-25 18:14:29 +0700

    Update anti_user 3.py

--- PyroUbot/modules/anti_user 3.py
+++ PyroUbot/modules/anti_user 3.py
@@ -117,7 +117,7 @@ async def display_blacklist(client, message):
         await message.reply(r)
        
 # Mengatur dan memproses perintah antiuser (on/off)
-@PY.UBOT("antipm")
+@PY.UBOT("antipc")
 @PY.ULTRA
 async def _(client, message):
     ggl = await EMO.GAGAL(client)
@@ -136,73 +136,79 @@ async def _(client, message):
 
 
 # Menambahkan pengguna ke dalam blacklist berdasarkan chat pribadi
-@PY.UBOT("nopc")
+@PY.UBOT("tambah")
 @PY.ULTRA
 async def add_user_to_blacklist(c, m):
     brhsl = await EMO.BERHASIL(c)
     ggl = await EMO.GAGAL(c)
 
+    # Mengecek apakah format perintah sudah benar (perlu user_id atau reply)
     if len(m.command) != 2 and not m.reply_to_message:
         await m.reply_text(f"<blockquote>{ggl}<b>ғᴏʀᴍᴀᴛ [ᴜsᴇʀ_ɪᴅ/ʀᴇᴘʟʏ]</b></blockquote>", quote=True)
         return
 
-    # Mendapatkan user ID berdasarkan reply atau command
+    # Mendapatkan user_id yang akan diblacklist, baik dari reply atau perintah
     if m.reply_to_message:
         user_id = m.reply_to_message.from_user.id
     else:
         user_id = int(m.command[1])
 
-    # Cek jika ID pengguna adalah pemilik bot, jika iya, maka tidak bisa ditambahkan
+    # Cek jika user_id adalah pemilik bot, tidak bisa diblacklist
     if user_id == OWNER_ID:
         return await m.reply(f"<blockquote>{ggl}<b>ɢᴜᴀ ʏᴀɴɢ ᴘᴜɴʏᴀ ʙᴏᴛ ᴛᴏᴅ!</b></blockquote>")
     
-    # Mendapatkan daftar blacklist pribadi pengguna bot
+    # Mendapatkan daftar ID pengguna yang diblacklist berdasarkan chat pribadi
     user_ids = await get_user_ids(c.me.id)
     if user_id not in user_ids:
+        # Menambahkan user_id ke dalam daftar blacklist
         user_ids.append(user_id)
         await user_collection.update_one({"_id": c.me.id}, {"$set": {"user_dia": user_ids}}, upsert=True)
         await m.reply_text(f"<blockquote>{brhsl}<b>ʙᴇʀʜᴀsɪʟ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴀɴᴛɪᴜsᴇʀ!!</b></blockquote>", quote=True)
     else:
+        # Jika user sudah ada dalam blacklist
         await m.reply_text(f"<blockquote>{ggl}<b>sᴜᴅᴀʜ ᴀᴅᴀ ᴅɪ ᴀɴᴛɪᴜsᴇʀ!!</b></blockquote>", quote=True)
 
 
-# Menghapus pengguna dari daftar blacklist berdasarkan chat pribadi
-@PY.UBOT("yespc")
+# Menghapus pengguna dari blacklist berdasarkan chat pribadi
+@PY.UBOT("hapus")
 @PY.ULTRA
 async def remove_user_from_blacklist(c, m):
     brhsl = await EMO.BERHASIL(c)
     ggl = await EMO.GAGAL(c)
 
+    # Mengecek apakah format perintah sudah benar (perlu user_id atau reply)
     if len(m.command) != 2 and not m.reply_to_message:
         await m.reply_text(f"<blockquote>{ggl}<b>ғᴏʀᴍᴀᴛ [ʀᴇᴘʟʏ_ᴘᴇꜱᴀɴ]</b></blockquote>", quote=True)
         return
 
-    # Mendapatkan user ID berdasarkan reply atau command
+    # Mendapatkan user_id yang akan dihapus dari blacklist
     if m.reply_to_message:
         user_id = m.reply_to_message.from_user.id
     else:
         user_id = int(m.command[1])
 
-    # Mendapatkan daftar pengguna yang ada dalam blacklist pribadi bot
+    # Mendapatkan daftar ID pengguna yang diblacklist berdasarkan chat pribadi
     user_ids = await get_user_ids(c.me.id)
     if user_id in user_ids:
+        # Menghapus user_id dari daftar blacklist
         user_ids.remove(user_id)
         await user_collection.update_one({"_id": c.me.id}, {"$set": {"user_dia": user_ids}}, upsert=True)
         await m.reply_text(f"<blockquote><b>ᴜsᴇʀ</b> : `{user_id}` \n<b>{brhsl}ᴛᴇʟᴀʜ ᴅɪʜᴀᴘᴜs ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ ᴀɴᴛɪᴜsᴇʀ</b></blockquote>", quote=True)
     else:
+        # Jika user tidak ada dalam blacklist
         await m.reply_text(f"<blockquote>{ggl}<b>ᴜsᴇʀ ᴛᴇʀsᴇʙᴜᴛ ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ ᴀɴᴛɪᴇʀ</b></blockquote>", quote=True)
 
 
 # Menampilkan daftar pengguna dalam blacklist
-@PY.UBOT("getantipm")
+@PY.UBOT("antipclist")
 @PY.ULTRA
 @PY.TOP_CMD
 async def display_blacklist(client, message):
     sks = await EMO.BERHASIL(client)
     try:
+        # Mendapatkan daftar ID pengguna yang ada dalam blacklist
         daftar = await get_user_ids(client.me.id)
         pesan = "\n".join(f"<blockquote><b>{sks}ᴅᴀғᴛᴀʀ ᴀɴᴛɪᴜsᴇʀ</b>\n\n ⌦ <code>`{x}`</code></blockquote>" for x in daftar)
         await message.reply(pesan)
     except Exception as r:
         await message.reply(r)
-   
