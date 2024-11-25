9cf0285c494283946015f8869bf528d2f216aed9
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2024-11-25 17:55:15 +0700

    Update anti_user 3.py

--- PyroUbot/modules/anti_user 3.py
+++ PyroUbot/modules/anti_user 3.py
@@ -115,3 +115,94 @@ async def display_blacklist(client, message):
         await message.reply(pesan)
     except Exception as r:
         await message.reply(r)
+       
+# Mengatur dan memproses perintah antiuser (on/off)
+@PY.UBOT("antipm")
+@PY.ULTRA
+async def _(client, message):
+    ggl = await EMO.GAGAL(client)
+    sks = await EMO.BERHASIL(client)
+    prs = await EMO.PROSES(client)
+    txt = await message.reply(f"{prs}<b>sᴇᴅᴀɴɢ ᴍᴇᴍᴘʀᴏsᴇs...</b>")
+    arg = get_arg(message)
+
+    if not arg or arg.lower() not in ["off", "on"]:
+        return await txt.edit(f"<blockquote><b>{ggl}ɢᴜɴᴀᴋᴀɴ ᴏɴ/ᴏғғ</b></blockquote>")
+
+    # Menyimpan status antiuser pada ID pengguna bot (client)
+    type = True if arg.lower() == "on" else False
+    await set_vars(client.me.id, "ON_OFF_ANTI_USER", type)
+    return await txt.edit(f"<blockquote><b>{sks}ᴀɴᴛɪᴜsᴇʀ ʙᴇʀʜᴀsɪʟ ᴅɪ sᴇᴛᴛɪɴɢs ᴋᴇ {type}</b></blockquote>")
+
+
+# Menambahkan pengguna ke dalam blacklist berdasarkan chat pribadi
+@PY.UBOT("nopc")
+@PY.ULTRA
+async def add_user_to_blacklist(c, m):
+    brhsl = await EMO.BERHASIL(c)
+    ggl = await EMO.GAGAL(c)
+
+    if len(m.command) != 2 and not m.reply_to_message:
+        await m.reply_text(f"<blockquote>{ggl}<b>ғᴏʀᴍᴀᴛ [ᴜsᴇʀ_ɪᴅ/ʀᴇᴘʟʏ]</b></blockquote>", quote=True)
+        return
+
+    # Mendapatkan user ID berdasarkan reply atau command
+    if m.reply_to_message:
+        user_id = m.reply_to_message.from_user.id
+    else:
+        user_id = int(m.command[1])
+
+    # Cek jika ID pengguna adalah pemilik bot, jika iya, maka tidak bisa ditambahkan
+    if user_id == OWNER_ID:
+        return await m.reply(f"<blockquote>{ggl}<b>ɢᴜᴀ ʏᴀɴɢ ᴘᴜɴʏᴀ ʙᴏᴛ ᴛᴏᴅ!</b></blockquote>")
+    
+    # Mendapatkan daftar blacklist pribadi pengguna bot
+    user_ids = await get_user_ids(c.me.id)
+    if user_id not in user_ids:
+        user_ids.append(user_id)
+        await user_collection.update_one({"_id": c.me.id}, {"$set": {"user_dia": user_ids}}, upsert=True)
+        await m.reply_text(f"<blockquote>{brhsl}<b>ʙᴇʀʜᴀsɪʟ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴀɴᴛɪᴜsᴇʀ!!</b></blockquote>", quote=True)
+    else:
+        await m.reply_text(f"<blockquote>{ggl}<b>sᴜᴅᴀʜ ᴀᴅᴀ ᴅɪ ᴀɴᴛɪᴜsᴇʀ!!</b></blockquote>", quote=True)
+
+
+# Menghapus pengguna dari daftar blacklist berdasarkan chat pribadi
+@PY.UBOT("yespc")
+@PY.ULTRA
+async def remove_user_from_blacklist(c, m):
+    brhsl = await EMO.BERHASIL(c)
+    ggl = await EMO.GAGAL(c)
+
+    if len(m.command) != 2 and not m.reply_to_message:
+        await m.reply_text(f"<blockquote>{ggl}<b>ғᴏʀᴍᴀᴛ [ʀᴇᴘʟʏ_ᴘᴇꜱᴀɴ]</b></blockquote>", quote=True)
+        return
+
+    # Mendapatkan user ID berdasarkan reply atau command
+    if m.reply_to_message:
+        user_id = m.reply_to_message.from_user.id
+    else:
+        user_id = int(m.command[1])
+
+    # Mendapatkan daftar pengguna yang ada dalam blacklist pribadi bot
+    user_ids = await get_user_ids(c.me.id)
+    if user_id in user_ids:
+        user_ids.remove(user_id)
+        await user_collection.update_one({"_id": c.me.id}, {"$set": {"user_dia": user_ids}}, upsert=True)
+        await m.reply_text(f"<blockquote><b>ᴜsᴇʀ</b> : `{user_id}` \n<b>{brhsl}ᴛᴇʟᴀʜ ᴅɪʜᴀᴘᴜs ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ ᴀɴᴛɪᴜsᴇʀ</b></blockquote>", quote=True)
+    else:
+        await m.reply_text(f"<blockquote>{ggl}<b>ᴜsᴇʀ ᴛᴇʀsᴇʙᴜᴛ ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ ᴀɴᴛɪᴇʀ</b></blockquote>", quote=True)
+
+
+# Menampilkan daftar pengguna dalam blacklist
+@PY.UBOT("getantipm")
+@PY.ULTRA
+@PY.TOP_CMD
+async def display_blacklist(client, message):
+    sks = await EMO.BERHASIL(client)
+    try:
+        daftar = await get_user_ids(client.me.id)
+        pesan = "\n".join(f"<blockquote><b>{sks}ᴅᴀғᴛᴀʀ ᴀɴᴛɪᴜsᴇʀ</b>\n\n ⌦ <code>`{x}`</code></blockquote>" for x in daftar)
+        await message.reply(pesan)
+    except Exception as r:
+        await message.reply(r)
+   
