caa83b6c9f30be06da96bc4a109301631bb982d3
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2024-12-03 02:28:42 +0700

    Update pm.py

--- PyroUbot/modules/pm.py
+++ PyroUbot/modules/pm.py
@@ -242,7 +242,7 @@ async def delete_old_message(message, msg_id):
     except:
         pass
 
-@PY.NO_PC("NOPC", ubot)
+@PY.NO_PM("NOPM", ubot)
 async def _(client, message):
     user = message.from_user
     nopm_on = await get_vars(client.me.id, "NOPM_STATUS")  # Ambil status NoPM dari database
