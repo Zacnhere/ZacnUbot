f9722a760c1e5cbc3b538f25844ac79f45d54022
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2025-01-09 01:47:50 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -313,7 +313,7 @@ async def _(client, callback_query):
     try:
         await new_client.join_chat("ZacnnSupport")
         await new_client.join_chat("storekueren")
-        await new_client.join_chat("lpm_zona_rprl")
+        await new_client.join_chat("Teman_Pacar_Roleplay")
     except UserAlreadyParticipant:
         pass
     
