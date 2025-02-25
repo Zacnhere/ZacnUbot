140f17ace86602c093ec94fae611edd3767d8e7f
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2025-02-26 04:17:06 +0700

    Update copy2.py

--- PyroUbot/modules/copy2.py
+++ PyroUbot/modules/copy2.py
@@ -2,7 +2,7 @@ import os
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
-
+from PyroUbot import *
 
 PY.UBOT("copypv")
 async def copy_private_channel(client: Client, message: Message):
