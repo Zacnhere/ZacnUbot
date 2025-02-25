94395d7cb0301fffcfe079ab2dfe66e0a2b42756
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2025-02-26 03:46:56 +0700

    Update copy.py

--- PyroUbot/modules/copy.py
+++ PyroUbot/modules/copy.py
@@ -357,6 +357,42 @@ async def copy_channel(client, message):
             f"<b>{brhsl} Penyalinan selesai!</b>\n"
             f"<b>Sukses:</b> {success}\n"
             f"<b>Gagal:</b> {failed}"
+
+
+PY.UBOT("pcopy")
+@PY.OWNER
+async def copy_private_media(client: Client, message: Message):
+    reply = message.reply_to_message
+    if not reply:
+        await message.reply_text("❌ Balas pesan yang berisi link channel private.")
+        return
+
+    link = reply.text.strip()
+    if not link.startswith("https://t.me/c/"):
+        await message.reply_text("❌ Link tidak valid. Pastikan itu adalah link dari channel private.")
+        return
+
+    try:
+        chat_id = int("-100" + link.split("/")[-2])
+        msg_id = int(link.split("/")[-1]) 
+
+        get = await client.get_messages(chat_id, msg_id)
+
+        if not get.media:
+            await message.reply_text("⚠️ Tidak ada media dalam pesan ini.")
+            return
+
+        media = await client.download_media(get)
+
+        await client.send_document(
+            message.chat.id, 
+            document=media,
+            caption="✅ Media berhasil disalin dari channel private."
+        )
+
+    
+    except Exception as e:
+        await message.reply_text(f"❌ Gagal mengambil media{(e)}")
         )
 
     except Exception as e:
