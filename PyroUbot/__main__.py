8968fde53dd8323467f71528c28a4b4937cc19f5
Zacnhere <168651993+Zacnhere@users.noreply.github.com>
2025-04-07 23:45:54 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -11,7 +11,7 @@ async def main():
         try:
             await asyncio.wait_for(ubot_.start(), timeout=10)
             await ubot_.join_chat("ZacnnSupport")
-            await ubot_.join_chat("Cari_Temen_Fwb")
+            await ubot_.join_chat("OfficialFirststeps")
         except asyncio.TimeoutError:
             print(f"Timeout: Ubot {int(_ubot['name'])} terjadi timeout!")
         except Exception:
